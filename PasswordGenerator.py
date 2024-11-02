17b9975db23e0ddadf51bd724b3e81501f0cf2ab
Leart Kraja <leartkraja02@gmail.com>
2024-11-02 17:56:04 +0100

    PasswordGenerator.py
    Random password generator using letters, numbers and symbols.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,14 +2,27 @@ import random
 
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers = '0123456789'
-password = str()
+symbols = ['.',',','!','#','$','&','+','='] #more symbols could be added just make sure they are of string type in the list
 
-while len(password) < 14 : #password length can be changed
-    if random.randint(0,1) == 0 :
-        if random.randint(0,1) == 0:
-            password = password + alphabet[random.randint(0,len(alphabet)-1)]
+def password_generator(n):
+    password = str()
+    while len(password) < n :
+        a = random.randint(0,2)
+        #randomizing the choices and the way those choices are added to the password string 
+        if a == 0 :
+            if random.randint(0,1) == 0:
+                password = password + alphabet[random.randint(0,len(alphabet)-1)]
+            else:
+                password = password + alphabet[random.randint(0,len(alphabet)-1)].lower()
+        elif a==1 :
+            password = password + numbers[random.randint(0,len(numbers)-1)]
         else:
-            password = password + alphabet[random.randint(0,len(alphabet)-1)].lower()
-    else:
-        password = password + numbers[random.randint(0,len(numbers)-1)]
-print(password)
+            password = password + symbols[random.randint(0,len(symbols)-1)]
+    return password
+
+def main():
+    n = int(input("Input the number of characters you want your password to have:"))
+    output = password_generator(n)
+    print(output)
+
+main()
